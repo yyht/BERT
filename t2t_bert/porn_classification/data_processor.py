e178af3e30aa020bf66d2ca669ba394f72deb5b3
许皓天 <xuhaotian@xuhaotiandeMacBook-Pro.local>
2018-12-18 21:01:01 +0800

    modify intent

--- t2t_bert/porn_classification/data_processor.py
+++ t2t_bert/porn_classification/data_processor.py
@@ -23,60 +23,60 @@ FLAGS = flags.FLAGS
 
 ## Required parameters
 flags.DEFINE_string(
-    "train_file", None,
-    "Input TF example files (can be a glob or comma separated).")
+	"train_file", None,
+	"Input TF example files (can be a glob or comma separated).")
 
 flags.DEFINE_string(
-    "test_file", None,
-    "Input TF example files (can be a glob or comma separated).")
+	"test_file", None,
+	"Input TF example files (can be a glob or comma separated).")
 
 flags.DEFINE_string(
-    "train_result_file", None,
-    "Input TF example files (can be a glob or comma separated).")
+	"train_result_file", None,
+	"Input TF example files (can be a glob or comma separated).")
 
 flags.DEFINE_string(
-    "test_result_file", None,
-    "Input TF example files (can be a glob or comma separated).")
+	"test_result_file", None,
+	"Input TF example files (can be a glob or comma separated).")
 
 flags.DEFINE_string(
-    "vocab_file", None,
-    "Input TF example files (can be a glob or comma separated).")
+	"vocab_file", None,
+	"Input TF example files (can be a glob or comma separated).")
 
 flags.DEFINE_string(
-    "label_id", None,
-    "Input TF example files (can be a glob or comma separated).")
+	"label_id", None,
+	"Input TF example files (can be a glob or comma separated).")
 
 flags.DEFINE_bool(
-    "lower_case", None,
-    "Input TF example files (can be a glob or comma separated).")
+	"lower_case", None,
+	"Input TF example files (can be a glob or comma separated).")
 
 flags.DEFINE_int(
-    "max_length", None,
-    "Input TF example files (can be a glob or comma separated).")
+	"max_length", None,
+	"Input TF example files (can be a glob or comma separated).")
 
 def main(_):
 
 	tokenizer = tokenization.FullTokenizer(
-    	vocab_file=FLAGS.vocab_file, 
-    	do_lower_case=FLAGS.lower_case)
+		vocab_file=FLAGS.vocab_file, 
+		do_lower_case=FLAGS.lower_case)
 
-    classifier_data_api = classifier_processor.PornClassifierProcessor()
-    classifier_data_api.get_labels(FLAGS.label_id)
+	classifier_data_api = classifier_processor.PornClassifierProcessor()
+	classifier_data_api.get_labels(FLAGS.label_id)
 
-    train_examples = classifier_data_api.get_train_examples(FLAGS.train_file)
+	train_examples = classifier_data_api.get_train_examples(FLAGS.train_file)
 
-    write_to_tfrecords.convert_classifier_examples_to_features(train_examples,
-    														classifier_data_api.label2id,
-                                                        	FLAGS.max_length,
-                                                        	tokenizer,
-                                                        	FLAGS.train_result_file)
+	write_to_tfrecords.convert_classifier_examples_to_features(train_examples,
+															classifier_data_api.label2id,
+															FLAGS.max_length,
+															tokenizer,
+															FLAGS.train_result_file)
 
-    test_examples = classifier_data_api.get_train_examples(FLAGS.test_file)
-    write_to_tfrecords.convert_classifier_examples_to_features(test_examples,
-    														classifier_data_api.label2id,
-                                                        	FLAGS.max_length,
-                                                        	tokenizer,
-                                                        	FLAGS.train_result_file)
+	test_examples = classifier_data_api.get_train_examples(FLAGS.test_file)
+	write_to_tfrecords.convert_classifier_examples_to_features(test_examples,
+															classifier_data_api.label2id,
+															FLAGS.max_length,
+															tokenizer,
+															FLAGS.train_result_file)
 
 
 
