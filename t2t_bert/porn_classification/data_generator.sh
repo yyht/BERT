7a6ff6cf1bc204d0d90dadc482ee7511827fcaaa
yyht <htxu91@gmail.com>
2018-12-18 12:58:35 +0000

    modify data genertor

--- t2t_bert/porn_classification/data_generator.sh
+++ t2t_bert/porn_classification/data_generator.sh
@@ -1,9 +1,9 @@
 python data_processor.py \
-	--train_file \
-	--test_file \
-	--train_result_file \
-	--test_result_file \
-	--vocab_file \
-	--label_id \
-	--lower_case \
-	--max_length \
+	--train_file /data/xuht/porn/seqing_train_20180821 \
+	--test_file /data/xuht/porn/seqing_test_20180821 \
+	--train_result_file /data/xuht/porn/seqing_train_20180821_tf_records \
+	--test_result_file  /data/xuht/porn/seqing_test_20180821_tf_records\
+	--vocab_file /data/xuht/chinese_L-12_H-768_A-12/vocab.txt \
+	--label_id /data/xuht/porn/label_dict.json \
+	--lower_case True \
+	--max_length 128 \
