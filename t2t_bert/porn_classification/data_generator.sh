efb2d015668c5b408129da438d60f3f858b156be
许皓天 <xuhaotian@xuhaotiandeMacBook-Pro.local>
2018-12-19 09:09:35 +0800

    modify data generator.sh

--- t2t_bert/porn_classification/data_generator.sh
+++ t2t_bert/porn_classification/data_generator.sh
@@ -1,9 +1,9 @@
 python data_processor.py \
-	--train_file /data/xuht/websiteanalyze-data-seqing20180821/seqing_train_20180821 \
-	--test_file /data/xuht/websiteanalyze-data-seqing20180821/seqing_test_20180821 \
-	--train_result_file /data/xuht/websiteanalyze-data-seqing20180821/seqing_train_20180821_tf_records \
-	--test_result_file  /data/xuht/websiteanalyze-data-seqing20180821/seqing_test_20180821_tf_records\
-	--vocab_file /data/xuht/bert/chinese_L-12_H-768_A-12/vocab.txt \
-	--label_id /data/xuht/websiteanalyze-data-seqing20180821/label_dict.json \
+	--train_file /data/xuht/porn/seqing_train_20180821 \
+	--test_file /data/xuht/porn/seqing_test_20180821 \
+	--train_result_file /data/xuht/porn/seqing_train_20180821_tf_records \
+	--test_result_file  /data/xuht/porn/seqing_test_20180821_tf_records\
+	--vocab_file /data/xuht/chinese_L-12_H-768_A-12/vocab.txt \
+	--label_id /data/xuht/porn/label_dict.json \
 	--lower_case True \
 	--max_length 128 \
