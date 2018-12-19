6faf18e96ecb4d6f37e46463dc7166bb7841c294
yyht <htxu91@gmail.com>
2018-12-19 02:15:39 +0000

    modify train.sh for training epoch

--- t2t_bert/porn_classification/train.sh
+++ t2t_bert/porn_classification/train.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES="0" mpirun -np 1 \
- -H localhost:1 \
+CUDA_VISIBLE_DEVICES="0,1" mpirun -np 2 \
+ -H localhost:2 \
 python base_train.py \
  --config_file "/data/xuht/chinese_L-12_H-768_A-12/bert_config.json" \
  --init_checkpoint "/data/xuht/chinese_L-12_H-768_A-12/bert_model.ckpt" \
@@ -9,7 +9,7 @@ python base_train.py \
  --dev_file "/data/xuht/porn/seqing_test_20180821_tf_records" \
  --max_length 128 \
  --model_output "/data/xuht/porn/model" \
- --epoch 8 \
+ --epoch 5 \
  --num_classes 5 \
  --batch_size 32 \
  --if_shard "1" \
