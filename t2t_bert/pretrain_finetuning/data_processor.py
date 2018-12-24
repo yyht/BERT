5e9f567e96ba22a26780acb82b2862304775b687
天逸 <albert.xht@alibaba-inc.com>
2018-12-24 20:01:08 +0800

    modify maksed lm

--- t2t_bert/pretrain_finetuning/data_processor.py
+++ t2t_bert/pretrain_finetuning/data_processor.py
@@ -74,6 +74,14 @@ flags.DEFINE_integer(
 
 def main(_):
 
+	def per_seq_dupe_func(tokens_a, tokens_b, **kargs):
+
+		max_predictions_per_seq_actual = 2
+		
+		dupe_factor_actual = 2 * max_predictions_per_seq_actual
+
+		return max_predictions_per_seq_actual, dupe_factor_actual
+
 	tokenizer = tokenization.FullTokenizer(
 		vocab_file=FLAGS.vocab_file, 
 		do_lower_case=FLAGS.lower_case)
@@ -96,7 +104,7 @@ def main(_):
 			random_seed=2018,
 			feature_type=FLAGS.feature_type,
 			log_cycle=FLAGS.log_cycle,
-			per_seq_dupe_func=write_to_records_pretrain.per_seq_dupe_func
+			per_seq_dupe_func=per_seq_dupe_func
 		)
 
 	test_examples = classifier_data_api.get_train_examples(FLAGS.test_file)
@@ -113,7 +121,7 @@ def main(_):
 			random_seed=2018,
 			feature_type=FLAGS.feature_type,
 			log_cycle=FLAGS.log_cycle,
-			per_seq_dupe_func=write_to_records_pretrain.per_seq_dupe_func
+			per_seq_dupe_func=per_seq_dupe_func
 		)
 
 	print("==Succeeded in preparing masked lm with finetuning data for task-finetuning with masked lm regularization")
