e652b470d350dff0b10a3f2b07c96f27fd7815e0
天逸 <albert.xht@alibaba-inc.com>
2018-12-24 17:35:12 +0800

    modify run_horovod data path

--- run_horovod.sh
+++ run_horovod.sh
@@ -2,7 +2,7 @@ sudo docker run -it -d -p 8318:8888 --name uber_distributed_xuht_py3 \
 --privileged \
 -e LANG=C.UTF-8 \
 -v /home/albert.xht/source:/notebooks/source \
--v /home/albert.xht/data:/data/xuht \
+-v /gruntdata/albert.xht:/data/xuht \
 -p 6152:6006 \
 -p 7949:8080 \
 -p 8339:8891 \
