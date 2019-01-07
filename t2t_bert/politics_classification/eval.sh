abc4cae5ac9fdb011498a64b5d3e82c37814b89d
天逸 <albert.xht@alibaba-inc.com>
2019-01-07 13:27:02 +0800

    fix politics

--- t2t_bert/politics_classification/eval.sh
+++ t2t_bert/politics_classification/eval.sh
@@ -2,17 +2,17 @@ CUDA_VISIBLE_DEVICES="0" mpirun -np 1 \
  -H localhost:2 \
 python eval.py \
  --config_file "/data/xuht/chinese_L-12_H-768_A-12/bert_config.json" \
- --init_checkpoint "/data/xuht/websiteanalyze-data-seqing20180821/model/oqmrc_8.ckpt" \
+ --init_checkpoint "/data/xuht/politics/model/model.ckpt" \
  --vocab_file "/data/xuht/chinese_L-12_H-768_A-12/vocab.txt" \
- --label_id "/data/xuht/websiteanalyze-data-seqing20180821/label_dict.json" \
- --train_file "/data/xuht/websiteanalyze-data-seqing20180821/global_mining/mining_info.pkl" \
- --dev_file "/data/xuht/porn/seqing_test_20180821_tf_records" \
+ --label_id "/data/xuht/politics/label_dict.json" \
+ --train_file "/data/xuht/politics/global_mining/mining_info.pkl" \
+ --dev_file "/data/xuht/politics/test_tf_records" \
  --max_length 64 \
- --model_output "/data/xuht/websiteanalyze-data-seqing20180821/global_mining/" \
+ --model_output "/data/xuht/politics/global_mining/" \
  --epoch 5 \
  --num_classes 5 \
  --batch_size 32 \
  --if_shard "1" \
  --lower_case True \
- --eval_data_file "/data/xuht/websiteanalyze-data-seqing20180821/global_mining/eval.tfrecords"
+ --eval_data_file "/data/xuht/politics/global_mining/eval.tfrecords"
 
