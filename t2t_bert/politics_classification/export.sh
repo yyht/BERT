2228e7dce6cac80c6ff6e1ae417a5215265ddc06
天逸 <albert.xht@alibaba-inc.com>
2019-01-14 20:13:35 +0800

    fix

--- t2t_bert/politics_classification/export.sh
+++ t2t_bert/politics_classification/export.sh
@@ -2,7 +2,7 @@ python export.py \
  --config_file "/data/xuht/chinese_L-12_H-768_A-12/bert_config.json" \
  --model_dir "/data/xuht/politics/model/" \
  --label2id "/data/xuht/politics/label_dict.json" \
- --init_checkpoint "/data/xuht/politics/model/oqmrc.ckpt" \
+ --init_checkpoint "/data/xuht/politics/model/model.ckpt" \
  --max_length 128 \
  --export_path "/data/xuht/politics/export" \
  --export_type "2"
