3909081def75116afe73abb59b378b8d1b70aee1
天逸 <albert.xht@alibaba-inc.com>
2019-01-14 16:21:21 +0800

    ad politics and porn tf serving model

--- t2t_bert/politics_classification/export.sh
+++ t2t_bert/politics_classification/export.sh
@@ -1,5 +1,5 @@
 python export_model.py \
- --config_file "/data/xuht/multi_cased_L-12_H-768_A-12/bert_config.json" \
+ --config_file "/data/xuht/chinese_L-12_H-768_A-12/bert_config.json" \
  --model_dir "/data/xuht/politics/model/" \
  --label2id "/data/xuht/politics/label_dict.json" \
  --init_checkpoint "/data/xuht/politics/model/oqmrc.ckpt" \
