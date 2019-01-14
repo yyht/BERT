318eb3489e3599ee4fe41ece5a7a59e881cba4e8
天逸 <albert.xht@alibaba-inc.com>
2019-01-14 16:17:44 +0800

    add politics tf serving support

---
+++ t2t_bert/politics_classification/export.sh
@@ -0,0 +1,8 @@
+python export_model.py \
+ --config_file "/data/xuht/multi_cased_L-12_H-768_A-12/bert_config.json" \
+ --model_dir "/data/xuht/politics/model/" \
+ --label2id "/data/xuht/politics/label_dict.json" \
+ --init_checkpoint "/data/xuht/politics/model/oqmrc.ckpt" \
+ --max_length 128 \
+ --export_path "/data/xuht/politics/export" \
+ --export_type "2"
