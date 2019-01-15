ae12785198b04b107fd99e3c713636347a0fac61
天逸 <albert.xht@alibaba-inc.com>
2019-01-15 09:23:16 +0800

    fix

--- t2t_bert/multilingual/train.sh
+++ t2t_bert/multilingual/train.sh
@@ -8,8 +8,8 @@ python base_train.py \
  --train_file "/data/xuht/lazada/20190107/train_tf_records" \
  --dev_file "/data/xuht/lazada/20190107/test_tf_records" \
  --max_length 128 \
- --model_output "/data/xuht/lazada/20190107/model" \
- --epoch 3 \
+ --model_output "/data/xuht/lazada/20190107/model_1_15" \
+ --epoch 5 \
  --num_classes 4 \
  --batch_size 16 \
  --if_shard "1" \
