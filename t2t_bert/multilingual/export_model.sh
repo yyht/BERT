6151d697d011943263c823bcbf571930a42dd582
天逸 <albert.xht@alibaba-inc.com>
2019-01-15 10:32:23 +0800

    fix model export

--- t2t_bert/multilingual/export_model.sh
+++ t2t_bert/multilingual/export_model.sh
@@ -1,8 +1,8 @@
 python export_model.py \
  --config_file "/data/xuht/multi_cased_L-12_H-768_A-12/bert_config.json" \
- --model_dir "/data/xuht/lazada/model/" \
+ --model_dir "/data/xuht/lazada/20190107/model/" \
  --label2id "/data/xuht/lazada/label_dict.json" \
- --init_checkpoint "/data/xuht/lazada/model/oqmrc_2.ckpt" \
+ --init_checkpoint "/data/xuht/lazada/20190107/model/model.ckpt" \
  --max_length 128 \
- --export_path "/data/xuht/lazada/export" \
+ --export_path "/data/xuht/lazada/20190107/export" \
  --export_type "2"
