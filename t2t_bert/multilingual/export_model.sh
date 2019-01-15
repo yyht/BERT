8e3812822b481e80eeb1e934ecb4ae0016ee6029
天逸 <albert.xht@alibaba-inc.com>
2019-01-15 10:38:47 +0800

    add cuda_visible_devices=

--- t2t_bert/multilingual/export_model.sh
+++ t2t_bert/multilingual/export_model.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES="" python export_model.py \
  --config_file "/data/xuht/multi_cased_L-12_H-768_A-12/bert_config.json" \
  --model_dir "/data/xuht/lazada/20190107/model/" \
  --label2id "/data/xuht/lazada/label_dict.json" \
- --init_checkpoint "/data/xuht/lazada/20190107/model/model.ckpt" \
+ --init_checkpoint "/data/xuht/lazada/20190107/model/model_3.ckpt" \
  --max_length 128 \
  --export_path "/data/xuht/lazada/20190107/export" \
  --export_type "2"
