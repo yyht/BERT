fc7977f6643828a976cf46b4eeb7b0c33634604a
天逸 <albert.xht@alibaba-inc.com>
2019-01-21 20:33:46 +0800

    fix

--- t2t_bert/porn_classification/restore.py
+++ t2t_bert/porn_classification/restore.py
@@ -52,8 +52,8 @@ def main(_):
 		sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, 
                                         log_device_placement=True))
 				
-		# saver = tf.train.import_meta_graph(FLAGS.meta, clear_devices=True)
-		saver = tf.train.import_meta_graph(FLAGS.meta)
+		saver = tf.train.import_meta_graph(FLAGS.meta, clear_devices=True)
+		# saver = tf.train.import_meta_graph(FLAGS.meta)
 		print("==succeeded in loading meta graph==")
 		saver.restore(sess, FLAGS.input_checkpoint)
 		print("==succeeded in loading model==")
