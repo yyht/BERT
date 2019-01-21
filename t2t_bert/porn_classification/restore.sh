0f45342453ad9b1dc8a103df7f9a2f610f7fc7fc
天逸 <albert.xht@alibaba-inc.com>
2019-01-21 20:14:59 +0800

    fix

--- t2t_bert/porn_classification/restore.sh
+++ t2t_bert/porn_classification/restore.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES="" python restore.py \
 	--meta "/data/xuht/porn/model/oqmrc_8.ckpt.meta" \
-	--input_checkpoint "/data/xuht/porn/model/oqmrc_8" \
+	--input_checkpoint "/data/xuht/porn/model/oqmrc_8.ckpt" \
 	--output_checkpoint "/data/xuht/porn/restore_model/oqmrc_8.ckpt" \
