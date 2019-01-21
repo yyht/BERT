12b78cd3e8ba848f9272f9e4f475bbad05d014c0
天逸 <albert.xht@alibaba-inc.com>
2019-01-21 20:44:33 +0800

    fix

--- t2t_bert/porn_classification/export.sh
+++ t2t_bert/porn_classification/export.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES="" python export.py \
  --config_file "/data/xuht/chinese_L-12_H-768_A-12/bert_config.json" \
  --model_dir "/data/xuht/porn/model/" \
  --label2id "/data/xuht/porn/label_dict.json" \
- --init_checkpoint "/data/xuht/porn/model/oqmrc_8.ckpt" \
+ --init_checkpoint "/data/xuht/porn/restore_model/oqmrc_8.ckpt" \
  --max_length 128 \
  --export_path "/data/xuht/porn/export" \
  --export_type "2"
