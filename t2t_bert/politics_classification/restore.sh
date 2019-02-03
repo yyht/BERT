5e72c2b34eeb2b5d10cd31dff55f74d85b2550c2
天逸 <albert.xht@alibaba-inc.com>
2019-02-03 14:11:53 +0800

    add politics, multilingual model restore.sh

---
+++ t2t_bert/politics_classification/restore.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES="" python restore.py \
+	--meta "/data/xuht/politics/model/model.ckpt.meta" \
+	--input_checkpoint "/data/xuht/politics/model/model.ckpt" \
+	--output_checkpoint "/data/xuht/politics/restore_model/model.ckpt" \
+	--output_meta_graph "/data/xuht/politics/restore_model/model.ckpt"
