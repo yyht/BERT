52e451c96134ee94260000f92749e1bdd4e7343a
天逸 <albert.xht@alibaba-inc.com>
2019-02-13 15:04:22 +0800

    jd comment test

--- t2t_bert/pai_single_sentence_classification/local_test.sh
+++ t2t_bert/pai_single_sentence_classification/local_test.sh
@@ -2,16 +2,18 @@ CUDA_VISIBLE_DEVICES="0" python ps_train_eval.py \
  --config_file "/data/xuht/chinese_L-12_H-768_A-12/bert_config.json" \
  --init_checkpoint "/data/xuht/chinese_L-12_H-768_A-12/bert_model.ckpt" \
  --vocab_file "/data/xuht/chinese_L-12_H-768_A-12/vocab.txt" \
- --label_id "/data/xuht/politics/label_dict.json" \
- --train_file "/data/xuht/politics/train_tf_records" \
- --dev_file "/data/xuht/politics/test_tf_records" \
+ --label_id "/data/xuht/jd_comment/label_dict.json" \
+ --train_file "/data/xuht/jd_comment/train.tfrecords" \
+ --dev_file "/data/xuht/jd_comment/test.tfrecords" \
  --max_length 128 \
- --model_output "/data/xuht/politics/model/ps_test" \
+ --model_output "/data/xuht/jd_comment/model/ps_test" \
  --epoch 2 \
  --num_classes 2 \
  --batch_size 32 \
  --if_shard "1" \
  --opt_type "" \
- --is_debug True
+ --is_debug False \
+ --train_size 33033 \
+ --eval_size 8589
 
  
