2795b6f80e58b1349f69e839c10878784156f270
天逸 <albert.xht@alibaba-inc.com>
2019-02-13 11:45:42 +0800

    fix

--- t2t_bert/pai_single_sentence_classification/train_eval_fn.py
+++ t2t_bert/pai_single_sentence_classification/train_eval_fn.py
@@ -236,6 +236,7 @@ def train_eval_fn(FLAGS,
 
 					for key in loss_dict:
 						loss_dict[key] = 0.0
+					break
 			except tf.errors.OutOfRangeError:
 				print("==Succeeded in training model==")
 					
