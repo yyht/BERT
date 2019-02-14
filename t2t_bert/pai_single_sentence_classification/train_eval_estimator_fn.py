42222ca87eb32910235c0fd67c6b9596223a0bc0
天逸 <albert.xht@alibaba-inc.com>
2019-02-14 11:15:40 +0800

    fix

--- t2t_bert/pai_single_sentence_classification/train_eval_estimator_fn.py
+++ t2t_bert/pai_single_sentence_classification/train_eval_estimator_fn.py
@@ -166,14 +166,17 @@ def train_eval_fn(FLAGS,
 						model_fn=model_fn,
 						config=run_config)
 
-		train_spec = tf.estimator.TrainSpec(input_fn=train_features, 
-										max_steps=num_train_steps,
-										hooks=train_hooks)
+		model_estimator.train(input_fn=train_features, 
+										max_steps=num_train_steps)
 
-		eval_spec = tf.estimator.EvalSpec(input_fn=eval_features, 
-										steps=num_eval_steps,
-										hooks=eval_hooks)
+		# train_spec = tf.estimator.TrainSpec(input_fn=train_features, 
+		# 								max_steps=num_train_steps,
+		# 								hooks=train_hooks)
 
-		tf.estimator.train_and_evaluate(model_estimator, train_spec, eval_spec)
+		# eval_spec = tf.estimator.EvalSpec(input_fn=eval_features, 
+		# 								steps=num_eval_steps,
+		# 								hooks=eval_hooks)
+
+		# tf.estimator.train_and_evaluate(model_estimator, train_spec, eval_spec)
 		
 		
