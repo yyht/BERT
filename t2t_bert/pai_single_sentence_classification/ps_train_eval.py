92ba74c6e74c60d424595f913f17f314f76dbc78
天逸 <albert.xht@alibaba-inc.com>
2019-02-14 19:13:31 +0800

    add pai bin for training

--- t2t_bert/pai_single_sentence_classification/ps_train_eval.py
+++ t2t_bert/pai_single_sentence_classification/ps_train_eval.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys,os
 
-sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
-
 import tensorflow as tf
 import os
-from train_eval_sess_fn import train_eval_fn as sess_fn
-from train_eval_estimator_fn import train_eval_fn as estimator_fn
+from .train_eval_sess_fn import train_eval_fn as sess_fn
+from .train_eval_estimator_fn import train_eval_fn as estimator_fn
 
 flags = tf.flags
 
