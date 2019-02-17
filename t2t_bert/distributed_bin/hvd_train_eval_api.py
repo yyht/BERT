9d99ac00994b41afb8d431cd0b00511ba37ec3ee
天逸 <albert.xht@alibaba-inc.com>
2019-02-17 15:21:09 +0800

    add logging

--- t2t_bert/distributed_bin/hvd_train_eval_api.py
+++ t2t_bert/distributed_bin/hvd_train_eval_api.py
@@ -37,7 +37,8 @@ flags = tf.flags
 
 FLAGS = flags.FLAGS
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.logging.set_verbosity(tf.logging.INFO)
 
 flags.DEFINE_string("buckets", "", "oss buckets")
 
