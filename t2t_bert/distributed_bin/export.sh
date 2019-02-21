fe43a974ef38f81483a9a9e4ec0dced4128663ec
天逸 <albert.xht@alibaba-inc.com>
2019-02-21 17:35:30 +0800

    fix export.py that add tf.app.run()

--- t2t_bert/distributed_bin/export.sh
+++ t2t_bert/distributed_bin/export.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES="" python ./t2t_bert/distributed_bin/export.py \
  --local_buckets "/data/xuht" \
  --config_file "chinese_L-12_H-768_A-12/bert_config.json" \
  --model_dir "jd_comment/hvd/rule_data/model_sess/" \
- --label2id "jd_comment/label_dict.json" \
+ --label_id "jd_comment/label_dict.json" \
  --init_checkpoint "jd_comment/hvd/rule_data/model_sess/model-1032.ckpt" \
  --max_length 128 \
  --export_path "jd_comment/hvd/rule_data/model_sess/export" \
