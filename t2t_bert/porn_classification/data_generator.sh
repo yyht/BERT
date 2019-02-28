7deacbd47383482423fd687f8fb73605b48f1e59
天逸 <albert.xht@alibaba-inc.com>
2019-02-28 09:57:48 +0800

    remove train and test overlap

--- t2t_bert/porn_classification/data_generator.sh
+++ t2t_bert/porn_classification/data_generator.sh
@@ -1,13 +1,27 @@
 python data_processor.py \
-	--train_file /data/xuht/porn/rule/seqing_train_20180821 \
-	--test_file /data/xuht/porn/rule/seqing_test_20180821 \
-	--train_result_file /data/xuht/porn/seqing_train_20180821_tf_records \
-	--test_result_file  /data/xuht/porn/seqing_test_20180821_tf_records\
+	--train_file /data/xuht/porn/actual_data/seqing_train_20180821 \
+	--test_file /data/xuht/porn/actual_data/seqing_test_20180821 \
+	--train_result_file /data/xuht/porn/actual_data/normal/seqing_train_20190228_tf_records \
+	--test_result_file  /data/xuht/porn/actual_data/normal/seqing_test_20190228_tf_records\
 	--vocab_file /data/xuht/chinese_L-12_H-768_A-12/vocab.txt \
 	--label_id /data/xuht/porn/label_dict.json \
 	--lower_case True \
 	--max_length 128 \
 	--if_rule False \
+	--rule_word_dict /data/xuht/porn/rule/rule/phrases.json \
+	--rule_word_path /data/xuht/porn/rule/rule/mined_porn_domain_adaptation_v2.txt \
+	--rule_label_dict /data/xuht/porn/rule/rule/rule_label_dict.json \
+
+python data_processor.py \
+	--train_file /data/xuht/porn/actual_data/seqing_train_20180821 \
+	--test_file /data/xuht/porn/actual_data/seqing_test_20180821 \
+	--train_result_file /data/xuht/porn/actual_data/rule/seqing_train_20190228_tf_records \
+	--test_result_file  /data/xuht/porn/actual_data/rule/seqing_test_20190228_tf_records\
+	--vocab_file /data/xuht/chinese_L-12_H-768_A-12/vocab.txt \
+	--label_id /data/xuht/porn/label_dict.json \
+	--lower_case True \
+	--max_length 128 \
+	--if_rule True \
 	--rule_word_dict /data/xuht/porn/rule/rule/phrases.json \
 	--rule_word_path /data/xuht/porn/rule/rule/mined_porn_domain_adaptation_v2.txt \
 	--rule_label_dict /data/xuht/porn/rule/rule/rule_label_dict.json \
