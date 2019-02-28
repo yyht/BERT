4a381c99a7f63c5bc67c312c13c483be0e5b8c52
天逸 <albert.xht@alibaba-inc.com>
2019-02-28 09:59:43 +0800

    remove train and test overlap

--- t2t_bert/porn_classification/data_generator.sh
+++ t2t_bert/porn_classification/data_generator.sh
@@ -1,6 +1,6 @@
 python data_processor.py \
-	--train_file /data/xuht/porn/actual_data/seqing_train_20180821 \
-	--test_file /data/xuht/porn/actual_data/seqing_test_20180821 \
+	--train_file /data/xuht/porn/actual_data/seqing_test_albert_20190228 \
+	--test_file /data/xuht/porn/actual_data/seqing_test_albert_20190228 \
 	--train_result_file /data/xuht/porn/actual_data/normal/seqing_train_20190228_tf_records \
 	--test_result_file  /data/xuht/porn/actual_data/normal/seqing_test_20190228_tf_records\
 	--vocab_file /data/xuht/chinese_L-12_H-768_A-12/vocab.txt \
@@ -13,8 +13,8 @@ python data_processor.py \
 	--rule_label_dict /data/xuht/porn/rule/rule/rule_label_dict.json \
 
 python data_processor.py \
-	--train_file /data/xuht/porn/actual_data/seqing_train_20180821 \
-	--test_file /data/xuht/porn/actual_data/seqing_test_20180821 \
+	--train_file /data/xuht/porn/actual_data/seqing_test_albert_20190228 \
+	--test_file /data/xuht/porn/actual_data/seqing_test_albert_20190228 \
 	--train_result_file /data/xuht/porn/actual_data/rule/seqing_train_20190228_tf_records \
 	--test_result_file  /data/xuht/porn/actual_data/rule/seqing_test_20190228_tf_records\
 	--vocab_file /data/xuht/chinese_L-12_H-768_A-12/vocab.txt \
