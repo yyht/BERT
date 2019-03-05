13672bd0fad00d258761ca02363eeb2cd8efcebd
天逸 <albert.xht@alibaba-inc.com>
2019-03-06 00:23:06 +0800

    fi

--- t2t_bert/distributed_bin/hvd_api.sh
+++ t2t_bert/distributed_bin/hvd_api.sh
@@ -2,30 +2,35 @@ mpirun -np 2 \
  -H localhost:2 \
  python ./t2t_bert/distributed_bin/hvd_train_eval_api.py \
  --buckets "/data/xuht" \
- --config_file "chinese_L-12_H-768_A-12/bert_config.json" \
- --init_checkpoint "chinese_L-12_H-768_A-12/bert_model.ckpt" \
- --vocab_file "chinese_L-12_H-768_A-12/vocab.txt" \
+ --config_file "./data/textcnn/textcnn.json" \
+ --init_checkpoint "" \
+ --vocab_file "w2v/tencent_ai_lab/char_id.txt" \
  --label_id "porn/label_dict.json" \
- --train_file "porn/rule/seqing_train_20180821_tf_records" \
- --dev_file "porn/rule/seqing_test_20180821_tf_records" \
  --max_length 128 \
- --model_output "porn/rule/model/sess" \
- --epoch 2 \
+ --train_file "porn/clean_data/textcnn/train_tfrecords" \
+ --dev_file "porn/clean_data/textcnn/dev_tfrecords" \
+ --model_output "porn/clean_data/textcnn/model/estimator/all_reduce_4_adam_weight_0228/" \
+ --epoch 8 \
  --num_classes 5 \
- --batch_size 32 \
- --train_size 1402171 \
- --eval_size 145019 \
- --model_type bert \
- --if_shard "1" \
- --is_debug "1" \
+ --train_size 952213 \
+ --eval_size 238054 \
+ --batch_size 24 \
+ --model_type "textcnn" \
+ --if_shard 2 \
+ --is_debug 1 \
  --run_type "sess" \
- --opt_type "hvd" \
- --distribution_strategy "ParameterServerStrategy" \
- --rule_model "rule" \
+ --opt_type "all_reduce" \
+ --num_gpus 4 \
  --parse_type "parse_batch" \
+ --rule_model "normal" \
  --profiler "no" \
+ --train_op "adam" \
  --running_type "train" \
+ --cross_tower_ops_type "paisoar" \
+ --distribution_strategy "MirroredStrategy" \
  --load_pretrained "no" \
- --w2v_path ""
+ --w2v_path "w2v/tencent_ai_lab/char_w2v.txt" \
+ --with_char "no_char" \
+ --input_target "a"
 
 
