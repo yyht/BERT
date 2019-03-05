1ebb5f49dda70e5a5d81b60545b040d2faae2152
天逸 <albert.xht@alibaba-inc.com>
2019-03-05 10:26:09 +0800

    fix loadw2v with gensim compatiable format

--- t2t_bert/data_generator/load_w2v.py
+++ t2t_bert/data_generator/load_w2v.py
@@ -5,7 +5,10 @@ from collections import OrderedDict
 def load_pretrained_w2v(vocab_path, w2v_path):
 	with tf.gfile.Open(w2v_path) as frobj:
 		vector = []
-		for line in frobj:
+		for index, line in enumerate(frobj):
+			if index == 0:
+				vocab_size, vector_size = map(int, line.strip())
+				continue
 			vector.append(line.strip())
 
 	with tf.gfile.Open(vocab_path) as frobj:
