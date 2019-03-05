794fb2c9b7bde72bc8aa3ede1a5277f9e59d9761
天逸 <albert.xht@alibaba-inc.com>
2019-03-05 20:39:38 +0800

    fix

--- t2t_bert/model/textcnn/textcnn.py
+++ t2t_bert/model/textcnn/textcnn.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 from utils.textcnn import textcnn_utils
+from utils.bimpm import match_utils
 from utils.embed import integration_func
 from model.base_classify import base_model
 
