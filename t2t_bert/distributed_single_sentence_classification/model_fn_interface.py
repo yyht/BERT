b6011cc5355e28377244ec9ea6a4420097e78ddf
天逸 <albert.xht@alibaba-inc.com>
2019-03-08 19:51:40 +0800

    modify **kargs for model fn builder

--- t2t_bert/distributed_single_sentence_classification/model_fn_interface.py
+++ t2t_bert/distributed_single_sentence_classification/model_fn_interface.py
@@ -1,14 +1,14 @@
 try:
 	from .model_fn import model_fn_builder
-	from .model_distillation_fn import model_fn_builder as model_distillation_fn
+	from .model_distillation_fn import model_fn_builder as model_distillation_builder_fn
 except:
 	from model_fn import model_fn_builder
-	from model_distillation_fn import model_fn_builder as model_distillation_fn
+	from model_distillation_fn import model_fn_builder as model_distillation_builder_fn
 
 def model_fn_interface(FLAGS):
 	print("==apply {} model fn builder==".format(FLAGS.distillation))
 	if FLAGS.distillation == "distillation":
-		return model_distillation_fn
+		return model_distillation_builder_fn
 	elif FLAGS.distillation == "normal":
 
 		return model_fn_builder
