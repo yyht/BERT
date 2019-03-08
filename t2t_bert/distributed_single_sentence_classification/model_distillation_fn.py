efc1049d09b8dc7093e6fbd9e7f3bc409d03e4ee
天逸 <albert.xht@alibaba-inc.com>
2019-03-09 00:29:34 +0800

    fix distillation loss

--- t2t_bert/distributed_single_sentence_classification/model_distillation_fn.py
+++ t2t_bert/distributed_single_sentence_classification/model_distillation_fn.py
@@ -57,13 +57,14 @@ def model_fn_builder(
 											label_ids,
 											dropout_prob)
 
-		teacher_prob = tf.nn.log_softmax(tf.log(features["label_probs"]+1e-10) / kargs.get("temperature", 2.0))
+		teacher_logit = tf.nn.log_softmax(tf.log(features["label_probs"]+1e-10) / kargs.get("temperature", 2.0))
+		teacher_prob = tf.exp(teacher_logit)
 		kl_divergence = teacher_prob * logits
 		
-		label_loss = tf.reduce_sum(per_example_loss * features["label_ratio"]) / (1e-10+tf.reduce_sum(features["label_ratio"]))
-		distillation_loss = -tf.reduce_sum(kl_divergence, axis=-1)
+		label_loss = tf.reduce_sum(per_example_loss * features["label_ratio"], axis=-1) / (1e-10+tf.reduce_sum(features["label_ratio"]))
+		distillation_loss = -tf.reduce_sum(kl_divergence, axis=-1) * (1 - features["label_ratio"])
 
-		loss = label_loss + kargs.get("distillation_ratio", 1.0)*distillation_loss
+		loss = label_loss + kargs.get("distillation_ratio", 1.0)*tf.reduce_mean(distillation_loss)
 
 		model_io_fn = model_io.ModelIO(model_io_config)
 
