b08216a63dc911048a4285dcf5c5afa3d72695aa
天逸 <albert.xht@alibaba-inc.com>
2019-03-09 00:42:17 +0800

    modify distillation data generation

--- t2t_bert/distributed_bin/hvd_api.sh
+++ t2t_bert/distributed_bin/hvd_api.sh
@@ -10,7 +10,7 @@ mpirun -np 2 \
  --train_file "porn/clean_data/textcnn/distillation/train_tfrecords" \
  --dev_file "porn/clean_data/textcnn/distillation/dev_tfrecords" \
  --model_output "porn/clean_data/textcnn/model/estimator/distillation/all_reduce_4_adam_weight_0228/" \
- --epoch 8 \
+ --epoch 20 \
  --num_classes 5 \
  --train_size 952213 \
  --eval_size 238054 \
