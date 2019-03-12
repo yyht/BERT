04f988cf3fe4a7df61cba4717d8486fc6279c514
天逸 <albert.xht@alibaba-inc.com>
2019-03-12 20:07:39 +0800

    add textlstm model

--- t2t_bert/distributed_encoder/classifynet_encoder.py
+++ t2t_bert/distributed_encoder/classifynet_encoder.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 from model.textcnn import textcnn
-# from model.textlstm import textlstm
+from model.textlstm import textlstm
 
 def textcnn_encoder(model_config, features, labels, 
 			mode, target, reuse=None, **kargs):
@@ -24,22 +24,22 @@ def textcnn_encoder(model_config, features, labels,
 	model.build_encoder(input_ids, input_char_ids, is_training, reuse=reuse, **kargs)
 	return model
 
-# def textlstm_encoder(model_config, features, labels, 
-# 			mode, target, reuse=None, **kargs):
+def textlstm_encoder(model_config, features, labels, 
+			mode, target, reuse=None, **kargs):
 
-# 	if mode == tf.estimator.ModeKeys.TRAIN:
-# 		is_training = tf.constant(True)
-# 	else:
-# 		is_training = tf.constant(False)
+	if mode == tf.estimator.ModeKeys.TRAIN:
+		is_training = tf.constant(True)
+	else:
+		is_training = tf.constant(False)
 
-# 	if target:
-# 		input_ids = features["input_ids_{}".format(target)]
-# 		input_char_ids = features.get("input_char_ids_{}".format(target), None)
-# 	else:
-# 		input_ids = features["input_ids"]
-# 		input_char_ids = features.get("input_char_ids_{}".format(target), None)
+	if target:
+		input_ids = features["input_ids_{}".format(target)]
+		input_char_ids = features.get("input_char_ids_{}".format(target), None)
+	else:
+		input_ids = features["input_ids"]
+		input_char_ids = features.get("input_char_ids_{}".format(target), None)
 
-# 	model = textlstm.TextLSTM(model_config)
-# 	model.build_emebdder(input_ids, input_char_ids, is_training, reuse=reuse, **kargs)
-# 	model.build_encoder(input_ids, input_char_ids, is_training, reuse=reuse, **kargs)
-# 	return model
+	model = textlstm.TextLSTM(model_config)
+	model.build_emebdder(input_ids, input_char_ids, is_training, reuse=reuse, **kargs)
+	model.build_encoder(input_ids, input_char_ids, is_training, reuse=reuse, **kargs)
+	return model
