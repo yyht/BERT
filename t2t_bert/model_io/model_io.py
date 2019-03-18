bbd1b76c23b295d5510d40b9e76eeb51b6bf4091
天逸 <albert.xht@alibaba-inc.com>
2019-03-18 15:13:21 +0800

    fix

--- t2t_bert/model_io/model_io.py
+++ t2t_bert/model_io/model_io.py
@@ -57,8 +57,8 @@ class ModelIO(object):
 		tvars = model_io_utils.get_params(scope, **kargs)
 		return tvars
 
-	def count_params(self, **kargs):
-		return model_io_utils.count_variables(scope)
+	def count_params(self, scope, **kargs):
+		return model_io_utils.count_variables(scope, **kargs)
 
 	def print_params(self, tvars, string):
 		model_io_utils.print_params(tvars, string)
