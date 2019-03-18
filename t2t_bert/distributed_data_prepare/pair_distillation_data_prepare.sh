35bf185f5065d2cc798227f2069b63caf3d0fc24
天逸 <albert.xht@alibaba-inc.com>
2019-03-18 22:45:28 +0800

    fix

--- t2t_bert/distributed_data_prepare/pair_distillation_data_prepare.sh
+++ t2t_bert/distributed_data_prepare/pair_distillation_data_prepare.sh
@@ -3,9 +3,9 @@ python ./t2t_bert/distributed_data_prepare/bert_distillation_data_prepare.py \
 	--train_file lcqmc/data/LCQMC_train.json \
 	--dev_file lcqmc/data/LCQMC_dev.json \
 	--test_file lcqmc/data/LCQMC_test.json \
-	--train_result_file lcqmc/data/distillation/train_tfrecords \
-	--dev_result_file lcqmc/data/distillation/dev_tfrecords\
-	--test_result_file lcqmc/data/distillation/test_tfrecords\
+	--train_result_file lcqmc/data/distillation_unlabeled/train_tfrecords \
+	--dev_result_file lcqmc/data/distillation_unlabeled/dev_tfrecords\
+	--test_result_file lcqmc/data/distillation_unlabeled/test_tfrecords\
 	--supervised_distillation_file lcqmc/data/distillation/train_distillation.info \
 	--unsupervised_distillation_file lcqmc/data/distillation/dev_distillation.info \
 	--vocab_file w2v/tencent_ai_lab/char_id.txt \
@@ -19,6 +19,6 @@ python ./t2t_bert/distributed_data_prepare/bert_distillation_data_prepare.py \
 	--with_char "no" \
 	--char_len 5 \
 	--predefined_vocab_size 50000 \
-	--corpus_vocab_path lcqmc/data/distillation/char_id.txt \
+	--corpus_vocab_path lcqmc/data/distillation_unlabeled/char_id.txt \
 	--data_type "lcqmc" \
-	--if_add_unlabeled_distillation "no"
+	--if_add_unlabeled_distillation "yes"
