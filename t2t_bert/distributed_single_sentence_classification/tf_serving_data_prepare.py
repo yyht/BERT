ad8db390cf9922c0e92777020b0924de66ba0019
天逸 <albert.xht@alibaba-inc.com>
2019-03-26 23:44:03 +0800

    fix

--- t2t_bert/distributed_single_sentence_classification/tf_serving_data_prepare.py
+++ t2t_bert/distributed_single_sentence_classification/tf_serving_data_prepare.py
@@ -4,72 +4,58 @@ from bunch import Bunch
 from data_generator import tokenization
 from data_generator import tf_data_utils
 from data_generator import rule_detector
-from model_io import model_io
 import json
 
-tokenizer = tokenization.FullTokenizer(
-				vocab_file=FLAGS.vocab, 
+def full2half(s):
+	n = []
+	for char in s:
+		num = ord(char)
+		if num == 0x3000:
+			num = 32
+		elif 0xFF01 <= num <= 0xFF5E:
+			num -= 0xfee0
+		num = chr(num)
+		n.append(num)
+	return ''.join(n)
+
+def get_tokenizer(FLAGS, 
+				vocab_path,
+				**kargs):
+	if FLAGS.tokenizer == "bert":
+		tokenizer = tokenization.FullTokenizer(
+				vocab_file=vocab_path, 
 				do_lower_case=FLAGS.do_lower_case)
+	elif FLAGS.tokenizer == "jieba_char":
+		tokenizer = tokenization.Jieba_CHAR(
+							config=kargs.get("config", {}))
 
-rule_config = {
-    "keyword_path":FLAGS.keyword_path, #"/data/xuht/jd_comment/phrases.json",
-    "background_label":FLAGS.background_label, #"正常",
-    "label_dict":FLAGS.label_dict #"/data/xuht/jd_comment/rule_label_dict.json"
-}
-rule_api = rule_detector.RuleDetector(rule_config)
-rule_api.load(tokenizer)
+		with tf.gfile.Open(vocab_path, "r") as f:
+			lines = f.read().splitlines()
+			vocab_lst = []
+			for line in lines:
+				vocab_lst.append(line)
+			print(len(vocab_lst))
 
-def full2half(ustring):
-	rstring = ""
-	for uchar in ustring:
-		inside_code=ord(uchar)
-		if inside_code==0x3000:
-			inside_code=0x0020
-		else:
-			inside_code-=0xfee0
-		if inside_code<0x0020 or inside_code>0x7e:   
-			rstring += uchar
-		else:
-			rstring += unichr(inside_code)
-	return rstring
+		tokenizer.load_vocab(vocab_lst)
 
-def get_single_features(query, max_seq_length):
-	tokens_a = tokenizer.tokenize(query)
+	return tokenizer
 
-	if len(tokens_a) > max_seq_length - 2:
-		tokens_a = tokens_a[0:(max_seq_length - 2)]
+def get_bert_single_features(FLAGS,
+								tokenizer, 
+								query,
+								max_seq_length):
+
+	tokens_a = tokenizer.tokenize(full2half(query))
+	tokens_a = tokens_a[0:max_seq_length-2]
 
 	tokens = []
 	segment_ids = []
 	tokens.append("[CLS]")
 	segment_ids.append(0)
 
-	if FLAGS.segment_id_type == "normal":
-		for token in tokens_a:
-			tokens.append(token)
-			segment_ids.append(0)
-	elif FLAGS.segment_id_type == "rule":
-
-		if os.path.exists(FLAGS.add_word_path):
-			extra_word = json.load(open(FLAGS.add_word_path, "r"))
-			for item in extra_word:
-				rule_detector.keyword_detector.add_keyword(tokenizer.tokenize(item["word"]), [item["label"]])
-		if os.path.exists(FLAGS.delete_word_path):
-			delete_word = json.load(open(FLAGS.delete_word_path, "r"))
-			for item in delete_word:
-				rule_detector.keyword_detector.remove_keyword(tokenizer.tokenize(item["word"]))
-		rule_ids = rule_detector.infer(tokens_a) # input is tokenized list
-		tokens = []
-		segment_ids = []
-		tokens.append("[CLS]")
+	for token in tokens_a:
+		tokens.append(token)
 		segment_ids.append(0)
-		for index, token in enumerate(tokens_a):
-			tokens.append(token)
-			segment_ids.append(rule_ids[index])
-	else:
-		for token in tokens_a:
-			tokens.append(token)
-			segment_ids.append(0)
 	tokens.append("[SEP]")
 	segment_ids.append(0)
 
@@ -81,33 +67,76 @@ def get_single_features(query, max_seq_length):
 		input_ids.append(0)
 		input_mask.append(0)
 		segment_ids.append(0)
-	label_ids = 0
 
-	return {"input_ids":input_ids,
-			"input_mask":input_mask,
-			"segment_ids":segment_ids,
+	feature_dict = {"input_ids":input_ids_a,
+			"input_mask":input_mask_a,
+			"segment_ids":segment_ids_a,
 			"label_ids":[0]}
 
-def get_feeddict(FLAGS):
+	return feature_dict
 
-	with open(FLAGS.query_path, "r") as frobj:
-		query_dict_lst = json.load(frobj)
+def get_single_features(FLAGS,
+							tokenizer, 
+							query, 
+							max_seq_length):
 
-	query_lst = [item["query"] for item in query_dict_lst]
-	label_lst = [item["label"] for item in query_dict_lst]
+	tokens_a = tokenizer.tokenize(full2half(query))
+			
+	if len(tokens_a) > max_seq_length:
+		tokens_a = tokens_a[0:(max_seq_length)]
 
-	features = []
+	input_ids_a = tokenizer.convert_tokens_to_ids(tokens_a, max_seq_length)
+	if FLAGS.with_char == "char":
+		input_char_ids_a = tokenizer.covert_tokens_to_char_ids(tokens_a, 
+										max_seq_length, 
+										char_len=char_len)
+	else:
+		input_char_ids_a = None
 
-	for query in query_lst:
-		feature = get_single_features(query, FLAGS.max_length)
-		features.append(feature)
+	feature_dict = {
+		"input_ids_a":input_ids_a,
+		"input_ids_b":input_ids_b,
+		"label_ids":[0]
+	}
+	if input_char_ids_a:
+		feature_dict["input_char_ids_a"] = input_char_ids_a
+
+	return feature_dict
+
+def get_feeddict(FLAGS, vocab_path,
+				corpus_path):
+
+	tokenizer_api = get_tokenizer(FLAGS, vocab_path)
+
+	with open(corpus_path, "r") as frobj:
+
+		query_lst = []
+		for line in frobj:
+			query_lst.append(line.strip())
+
+		instances = []
+		for query in query_lst:
+			print(query)
+			if FLAGS.model_type == "bert":
+				feature_dict = get_bert_single_features(
+								FLAGS, 
+								tokenizer, 
+								query, 
+								FLAGS.max_seq_length)
+			elif FLAGS.model_type == "single_sentence_classification":
+				feature_dict = get_single_features(
+								FLAGS, 
+								tokenizer_api, 
+								query, 
+								FLAGS.max_seq_length)
+			print(feature_dict)
+			instances.append(feature_dict)
 
 	feed_dict = {
-		"instances":features,
+		"instances":instances,
 		"signature_name":FLAGS.signature_name,
 	}
 
 	return feed_dict
-
 
 
