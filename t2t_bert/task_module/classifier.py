2efe36c90307a57fe85fc076924798d03f1fc3cc
天逸 <albert.xht@alibaba-inc.com>
2019-04-25 11:12:06 +0800

    fix

--- t2t_bert/task_module/classifier.py
+++ t2t_bert/task_module/classifier.py
@@ -24,7 +24,7 @@ def classifier(config, pooled_output,
 	logits = tf.matmul(output_layer, output_weights, transpose_b=True)
 	logits = tf.nn.bias_add(logits, output_bias)
 
-	logits = tf.log_softmax(logits)
+	logits = tf.nn.log_softmax(logits)
 
 	if config.get("label_type", "single_label") == "single_label":
 		if config.get("loss", "entropy") == "entropy":
