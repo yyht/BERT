930bf4c59e8a85d049a81ad3833b258462b2f4e7
天逸 <albert.xht@alibaba-inc.com>
2019-04-29 16:07:57 +0800

    add multitask data processor

--- t2t_bert/distributed_data_prepare/multitask_data_prepare.sh
+++ t2t_bert/distributed_data_prepare/multitask_data_prepare.sh
@@ -1,6 +1,6 @@
 python ./t2t_bert/distributed_data_prepare/multitask_classification_data_prepare.py \
 	--buckets /data/xuht \
 	--multitask_dict ./t2t_bert/distributed_multitask/multi_task.json \
-	--vocab_file w2v/tencent_ai_lab/char_id.txt \
+	--vocab_file ./data/chinese_L-12_H-768_A-12/vocab.txt \
 	--lower_case True \
 	--max_length 128
