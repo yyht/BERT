94c030321cc5a7716cd97b00ea123a6c6272f334
天逸 <albert.xht@alibaba-inc.com>
2019-10-31 18:31:29 +0800

    modify tpu eval script

--- t2t_bert/optimizer/optimizer_utils.py
+++ t2t_bert/optimizer/optimizer_utils.py
@@ -336,7 +336,7 @@ class LAMBOptimizer_v2(tf.train.Optimizer):
 
 	def _get_variable_name(self, param_name):
 		"""Get the variable name from the tensor name."""
-		m = re.match("^(.*):\\d+$", six.ensure_str(param_name))
+		m = re.match("^(.*):\\d+$", (param_name))
 		if m is not None:
 			param_name = m.group(1)
 		return param_name
