05d2ce6453a070080e50ae9d29e5e4d06856160d
yyht <10343351332@qq.com>
2019-11-11 11:53:57 +0000

    add bert-base eval

--- t2t_bert/distributed_bin/tpu_pretrain_electra_script.sh
+++ t2t_bert/distributed_bin/tpu_pretrain_electra_script.sh
@@ -7,7 +7,7 @@ nohup python ./t2t_bert/distributed_bin/tpu_train_eval_api.py \
 	--max_length 512 \
 	--train_file "pretrain_single_random_hard_gan/chunk_0.tfrecords,pretrain_single_random_hard_gan/chunk_1.tfrecords,pretrain_single_random_hard_gan/chunk_2.tfrecords,pretrain_single_random_hard_gan/chunk_3.tfrecords,pretrain_single_random_hard_gan/chunk_4.tfrecords,pretrain_single_random_hard_gan/chunk_5.tfrecords,pretrain_single_random_hard_gan/chunk_6.tfrecords,pretrain_single_random_hard_gan/chunk_7.tfrecords,pretrain_single_random_hard_gan/chunk_8.tfrecords,pretrain_single_random_hard_gan/chunk_9.tfrecords,pretrain_single_random_hard_gan/chunk_10.tfrecords,pretrain_single_random_hard_gan/chunk_11.tfrecords,pretrain_single_random_hard_gan/chunk_12.tfrecords,pretrain_single_random_hard_gan/chunk_13.tfrecords,pretrain_single_random_hard_gan/chunk_14.tfrecords,pretrain_single_random_hard_gan/chunk_15.tfrecords,pretrain_single_random_hard_gan/chunk_16.tfrecords,pretrain_single_random_hard_gan/chunk_17.tfrecords" \
 	--dev_file "pretrain_single_random_hard_gan/chunk_18.tfrecords,pretrain_single_random_hard_gan/chunk_19.tfrecords" \
-	--model_output "model/electra_bert_tiny" \
+	--model_output "model/electra_bert_tiny_gen_bert_tiny_dis" \
 	--epoch 15 \
 	--num_classes 2 \
 	--train_size 11000000 \
