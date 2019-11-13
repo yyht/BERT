011847cfbdf823078237d97e457d8cc05523ea76
天逸 <albert.xht@alibaba-inc.com>
2019-11-13 11:14:37 +0800

    add ori replace mask

--- t2t_bert/pretrain_finetuning/run_tensorboard.sh
+++ t2t_bert/pretrain_finetuning/run_tensorboard.sh
@@ -1,7 +1,7 @@
 odpscmd=$1
 pai_command="
 	pai -name tensorboard
-		-DsummaryDir='oss://alg-misc/BERT/bert_pretrain/open_domain/pretrain_single_random_hard_gan/electra_debug_only_mask/?role_arn=acs:ram::1265628042679515:role/yuefeng2&host=cn-hangzhou.oss-internal.aliyun-inc.com'
+		-DsummaryDir='oss://alg-misc/BERT/bert_pretrain/open_domain/pretrain_single_random_hard_gan/electra_debug_only_mask_generator/?role_arn=acs:ram::1265628042679515:role/yuefeng2&host=cn-hangzhou.oss-internal.aliyun-inc.com'
 "
 
 echo "${pai_command}"
