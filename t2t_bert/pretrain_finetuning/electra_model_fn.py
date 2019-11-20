c7e4dc0b222a38230ab397af3b15eee964ae4b40
天逸 <albert.xht@alibaba-inc.com>
2019-11-20 13:04:09 +0800

    add cpc utils and electra model tied embedding

--- t2t_bert/pretrain_finetuning/electra_model_fn.py
+++ t2t_bert/pretrain_finetuning/electra_model_fn.py
@@ -80,10 +80,11 @@ def classifier_model_fn_builder(
 			else:
 				tvars.append(var)
 
-		tvars.extend(discriminator_dict['tvars'])
+		# tvars.extend(discriminator_dict['tvars'])
 		if kargs.get('joint_train', '0') == '1':
 			tvars.extend(generator_fn['tvars'])
 			loss += generator_dict['loss']
+		tvars = list(set(tvars))
 
 		var_checkpoint_dict_list = []
 		for key in init_checkpoint_dict:
