ba0f3ad0e77776be0d77ebec65ad76c341c6f74a
yyht <htxu91@gmail.com>
2019-11-22 02:32:00 +0000

    add joint training

--- t2t_bert/pretrain_finetuning/electra_model_fn.py
+++ t2t_bert/pretrain_finetuning/electra_model_fn.py
@@ -81,7 +81,8 @@ def classifier_model_fn_builder(
 		#		tvars.append(var)
 
 		tvars.extend(discriminator_dict['tvars'])
-		if kargs.get('joint_train', '0') == '1':
+		if kargs.get('joint_train', '1') == '1':
+                        tf.logging.info("****** joint generator and discriminator training *******")
 			tvars.extend(generator_fn['tvars'])
 			loss += generator_dict['loss']
 		tvars = list(set(tvars))
