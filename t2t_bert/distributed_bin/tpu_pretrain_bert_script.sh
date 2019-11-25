38f996986770f60290355ee5e3140a05e1a3dfb2
yyht <10343351332@qq.com>
2019-11-25 12:11:50 +0000

    electra joint trianing

--- t2t_bert/distributed_bin/tpu_pretrain_bert_script.sh
+++ t2t_bert/distributed_bin/tpu_pretrain_bert_script.sh
@@ -1,18 +1,18 @@
 nohup python ./t2t_bert/distributed_bin/tpu_train_eval_api.py \
 	--buckets "gs://yyht_source/pretrain" \
-	--config_file "./data/roberta_zh_l12/bert_config_tiny.json" \
+	--config_file "./data/roberta_zh_l12/bert_config.json" \
 	--init_checkpoint "" \
 	--vocab_file "./data/chinese_L-12_H-768_A-12/vocab.txt" \
 	--label_id "./data/lcqmc/label_dict.json" \
 	--max_length 512 \
-	--train_file "data_single_random/chunk_0.tfrecords,data_single_random/chunk_1.tfrecords,data_single_random/chunk_2.tfrecords,data_single_random/chunk_3.tfrecords,data_single_random/chunk_4.tfrecords,data_single_random/chunk_5.tfrecords,data_single_random/chunk_6.tfrecords,data_single_random/chunk_7.tfrecords,data_single_random/chunk_8.tfrecords,data_single_random/chunk_9.tfrecords,data_single_random/chunk_10.tfrecords,data_single_random/chunk_11.tfrecords,data_single_random/chunk_12.tfrecords,data_single_random/chunk_13.tfrecords,data_single_random/chunk_14.tfrecords,data_single_random/chunk_15.tfrecords,data_single_random/chunk_16.tfrecords,data_single_random/chunk_17.tfrecords" \
-	--dev_file "data_single_random/chunk_18.tfrecords,data_single_random/chunk_19.tfrecords" \
-	--model_output "model/bert_tiny_with_single_random_adam_decay_15" \
+	--train_file "data_single_hard_gan/chunk_0.tfrecords,data_single_hard_gan/chunk_1.tfrecords,data_single_hard_gan/chunk_2.tfrecords,data_single_hard_gan/chunk_3.tfrecords,data_single_hard_gan/chunk_4.tfrecords,data_single_hard_gan/chunk_5.tfrecords,data_single_hard_gan/chunk_6.tfrecords,data_single_hard_gan/chunk_7.tfrecords,data_single_hard_gan/chunk_8.tfrecords,data_single_hard_gan/chunk_9.tfrecords,data_single_hard_gan/chunk_10.tfrecords,data_single_hard_gan/chunk_11.tfrecords,data_single_hard_gan/chunk_12.tfrecords,data_single_hard_gan/chunk_13.tfrecords,data_single_hard_gan/chunk_14.tfrecords,data_single_hard_gan/chunk_15.tfrecords,data_single_hard_gan/chunk_16.tfrecords,data_single_hard_gan/chunk_17.tfrecords" \
+	--dev_file "data_single_hard_gan/chunk_18.tfrecords,data_single_hard_gan/chunk_19.tfrecords" \
+	--model_output "model/bert_base_with_single_random_adam_decay_15" \
 	--epoch 15 \
 	--num_classes 2 \
 	--train_size 11000000 \
 	--eval_size 1100000 \
-	--batch_size 1200 \
+	--batch_size 384 \
 	--model_type "bert" \
 	--if_shard 1 \
 	--is_debug 1 \
@@ -26,7 +26,7 @@ nohup python ./t2t_bert/distributed_bin/tpu_train_eval_api.py \
 	--ln_type "postln" \
 	--warmup "warmup" \
 	--decay "decay" \
-	--init_lr 5e-4 \
+	--init_lr 2e-4 \
 	--do_train true \
 	--tpu_name $TPU_NAME \
 	--mode "pretrain"
