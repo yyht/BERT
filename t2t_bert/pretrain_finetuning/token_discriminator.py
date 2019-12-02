5a1756ce0ee57312a90253682c5823cbe2bffb3f
天逸 <albert.xht@alibaba-inc.com>
2019-12-02 12:56:42 +0800

    add masked only and modify all mask ids' bugs

--- t2t_bert/pretrain_finetuning/token_discriminator.py
+++ t2t_bert/pretrain_finetuning/token_discriminator.py
@@ -73,7 +73,7 @@ def classifier(config, seq_output,
 			tmp_ori_sampled_ids = tf.cast(ori_sampled_ids, tf.int32) * 
 			tf.logging.info("****** normal 2-D sampled_ids *******")
 
-		masked_not_equal_mask = tf.cast(tf.not_equal(input_ids, ori_sampled_ids), tf.int32)
+		masked_not_equal_mask = tf.cast(tf.not_equal(input_ids, tmp_ori_sampled_ids), tf.int32)
 		masked_not_equal_mask *= tf.cast(input_mask, tf.int32)
 	else:
 		masked_not_equal_mask = None
