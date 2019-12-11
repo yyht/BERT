fix gradient reversal layer

t2t_bert/distributed_single_sentence_classification/model_distillation_adv_adaptation.py
@@ -76,7 +76,7 @@ def diff_loss(shared_feat, task_feat):
 	correlation_matrix = tf.matmul(
 		task_feat, shared_feat, transpose_a=True)
 
-	cost = tf.reduce_mean(tf.square(correlation_matrix)) * 0.01
+	cost = tf.reduce_mean(tf.square(correlation_matrix))
 	cost = tf.where(cost > 0, cost, 0, name='value')
 
 	assert_op = tf.Assert(tf.is_finite(cost), [cost])

t2t_bert/pretrain_finetuning/discriminator_gumbel.py
@@ -22,7 +22,7 @@ class FlipGradientBuilder(object):
 
 	def __call__(self, x, l=1.0):
 		grad_name = "FlipGradient%d" % self.num_calls
-		@ops.RegisterGrad                                                                                   ient(grad_name)
+		@ops.RegisterGradient(grad_name)
 		def _flip_gradients(op, grad):
 			return [tf.negative(grad) * l]
 		