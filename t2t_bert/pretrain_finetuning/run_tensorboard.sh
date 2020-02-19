42ea3bca972eba6ecdac159a3aba7dcb812a758a
天逸 <albert.xht@alibaba-inc.com>
2020-02-19 18:42:23 +0800

    fix invariant-shape mask sampling

--- t2t_bert/pretrain_finetuning/run_tensorboard.sh
+++ t2t_bert/pretrain_finetuning/run_tensorboard.sh
@@ -1,7 +1,7 @@
 odpscmd=$1
 pai_command="
 	pai -name tensorboard
-		-DsummaryDir='oss://alg-misc/BERT/bert_pretrain/open_domain/pretrain_single_random_debug_gan/relgan/all_mask_unnormalized_soft_constant_0.01_JS_test_span_mask/?role_arn=acs:ram::1265628042679515:role/yuefeng2&host=cn-hangzhou.oss-internal.aliyun-inc.com'
+		-DsummaryDir='oss://alg-misc/BERT/bert_pretrain/open_domain/pretrain_single_random_debug_gan/relgan/all_mask_unnormalized_soft_constant_0.01_JS_test_mixed_mask_v3/?role_arn=acs:ram::1265628042679515:role/yuefeng2&host=cn-hangzhou.oss-internal.aliyun-inc.com'
 "
 
 echo "${pai_command}"
