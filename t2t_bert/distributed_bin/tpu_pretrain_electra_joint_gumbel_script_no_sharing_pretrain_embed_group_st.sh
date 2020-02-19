18d9ad93aca960340b19b1948b7ba9c5307ca79a
yyht <htxu91@gmail.com>
2020-02-19 02:53:56 +0000

    eval

--- t2t_bert/distributed_bin/tpu_pretrain_electra_joint_gumbel_script_no_sharing_pretrain_embed_group_st.sh
+++ t2t_bert/distributed_bin/tpu_pretrain_electra_joint_gumbel_script_no_sharing_pretrain_embed_group_st.sh
@@ -8,11 +8,11 @@ nohup python ./t2t_bert/distributed_bin/tpu_train_eval_api.py \
 	--train_file "data_single_hard_gan/chunk_0.tfrecords,data_single_hard_gan/chunk_1.tfrecords,data_single_hard_gan/chunk_2.tfrecords,data_single_hard_gan/chunk_3.tfrecords,data_single_hard_gan/chunk_4.tfrecords,data_single_hard_gan/chunk_5.tfrecords,data_single_hard_gan/chunk_6.tfrecords,data_single_hard_gan/chunk_7.tfrecords,data_single_hard_gan/chunk_8.tfrecords,data_single_hard_gan/chunk_9.tfrecords,data_single_hard_gan/chunk_10.tfrecords,data_single_hard_gan/chunk_11.tfrecords,data_single_hard_gan/chunk_12.tfrecords,data_single_hard_gan/chunk_13.tfrecords,data_single_hard_gan/chunk_14.tfrecords,data_single_hard_gan/chunk_15.tfrecords,data_single_hard_gan/chunk_16.tfrecords,data_single_hard_gan/chunk_17.tfrecords" \
 	--dev_file "data_single_hard_gan/chunk_18.tfrecords,data_single_hard_gan/chunk_19.tfrecords" \
 	--model_output "model/group/st/constant_0.01_greedy/electra_bert_tiny_gen_bert_tiny_dis_joint_gumbel_no_sharing_pretrained_embedding" \
-	--epoch 15 \
+	--epoch 25 \
 	--num_classes 2 \
 	--train_size 11000000 \
 	--eval_size 1100000 \
-	--batch_size 128 \
+	--batch_size 256 \
 	--model_type "albert" \
 	--if_shard 1 \
 	--is_debug 1 \
