5084186f4f5db55048d5ecd39d0a2f640edd2876
yyht <htxu91@gmail.com>
2020-03-13 08:38:15 +0000

    fix

--- t2t_bert/pretrain_finetuning/trf_gpt_noise.py
+++ t2t_bert/pretrain_finetuning/trf_gpt_noise.py
@@ -157,7 +157,7 @@ def model_fn_builder(
 											gumbel_temp=temperature,
 											estimator=noise_estimator_type,
 											back_prop=True,
-											swap_memory=False,
+											swap_memory=True,
 											seq_type=kargs.get("seq_type", "seq2seq"),
 											mask_type=kargs.get("mask_type", "left2right"),
 											attention_type=kargs.get('attention_type', 'normal_attention'),
