18c2fef359adb68ed5e1f845b098d49acb3a823f
天逸 <albert.xht@alibaba-inc.com>
2020-03-24 17:38:40 +0800

    fix

--- t2t_bert/pretrain_finetuning/run_tensorboard.sh
+++ t2t_bert/pretrain_finetuning/run_tensorboard.sh
@@ -1,7 +1,7 @@
 odpscmd=$1
 pai_command="
 	pai -name tensorboard
-		-DsummaryDir='oss://alg-misc/BERT/bert_pretrain/open_domain/pretrain_single_random_debug_gan/trf_ebm_tiny/mlm_sample/mi_ebm_logv_constant_with_noise_v1_noise_pretrain_new/?role_arn=acs:ram::1265628042679515:role/yuefeng2&host=cn-hangzhou.oss-internal.aliyun-inc.com'
+		-DsummaryDir='oss://alg-misc/BERT/bert_pretrain/open_domain/pretrain_single_random_debug_gan/trf_ebm_tiny/mlm_sample/mi_ebm_logv_constant_with_noise_v1_noise_jsd/?role_arn=acs:ram::1265628042679515:role/yuefeng2&host=cn-hangzhou.oss-internal.aliyun-inc.com'
 "
 
 echo "${pai_command}"
