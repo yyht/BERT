557f882708eafbf7d540898b7eba4f162b5f893e
yyht <htxu91@gmail.com>
2020-04-17 14:59:53 +0000

    fix

--- t2t_bert/distributed_bin/tpu_pretrain_bert_tiny_script.sh
+++ t2t_bert/distributed_bin/tpu_pretrain_bert_tiny_script.sh
@@ -12,7 +12,7 @@ nohup python ./t2t_bert/distributed_bin/tpu_train_eval_api.py \
 	--num_classes 2 \
 	--train_size 11000000 \
 	--eval_size 1100000 \
-	--batch_size 384 \
+	--batch_size 512 \
 	--model_type "bert" \
 	--if_shard 1 \
 	--is_debug 1 \
