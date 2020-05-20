a408ce03c2b54be6ed202f0aca0207d6afd78a90
yyht <htxu91@gmail.com>
2020-05-20 10:11:28 +0000

    fix

--- t2t_bert/distributed_bin/tpu_pretrain_gatedcnn.sh
+++ t2t_bert/distributed_bin/tpu_pretrain_gatedcnn.sh
@@ -7,7 +7,7 @@ nohup python ./t2t_bert/distributed_bin/tpu_train_eval_api.py \
 	--max_length 256 \
 	--train_file "mnli_pretrain/multinli.train.ar.tsv.256.tfrecords,mnli_pretrain/multinli.train.bg.tsv.256.tfrecords,mnli_pretrain/multinli.train.de.tsv.256.tfrecords,mnli_pretrain/multinli.train.el.tsv.256.tfrecords,mnli_pretrain/multinli.train.en.tsv.256.tfrecords,mnli_pretrain/multinli.train.es.tsv.256.tfrecords,mnli_pretrain/multinli.train.fr.tsv.256.tfrecords,mnli_pretrain/multinli.train.hi.tsv.256.tfrecords,mnli_pretrain/multinli.train.ru.tsv.256.tfrecords,mnli_pretrain/multinli.train.sw.tsv.256.tfrecords,mnli_pretrain/multinli.train.th.tsv.256.tfrecords,mnli_pretrain/multinli.train.tr.tsv.256.tfrecords,mnli_pretrain/multinli.train.ur.tsv.256.tfrecords,mnli_pretrain/multinli.train.vi.tsv.256.tfrecords,mnli_pretrain/multinli.train.zh.tsv.256.tfrecords,mnli_pretrain/train.json.256.tfrecords.not_selected,mnli_pretrain/train.json.256.tfrecords.selected,mnli_pretrain/xquad.ar.json.256.tfrecords,mnli_pretrain/xquad.de.json.256.tfrecords,mnli_pretrain/xquad.el.json.256.tfrecords,mnli_pretrain/xquad.en.json.256.tfrecords,mnli_pretrain/xquad.es.json.256.tfrecords,mnli_pretrain/xquad.hi.json.256.tfrecords,mnli_pretrain/xquad.ru.json.256.tfrecords,mnli_pretrain/xquad.th.json.256.tfrecords,mnli_pretrain/xquad.tr.json.256.tfrecords,mnli_pretrain/xquad.vi.json.256.tfrecords,mnli_pretrain/xquad.zh.json.256.tfrecords,mnli_pretrain/xquad.zh1.json.256.tfrecords" \
 	--dev_file "data_single_hard_gan/chunk_18.tfrecords,data_single_hard_gan/chunk_19.tfrecords" \
-	--model_output "model/gatedcnn_seq/gatedcnn_seq" \
+	--model_output "model/gatedcnn_seq/gatedcnn_seq_v1" \
 	--epoch 50 \
 	--num_classes 2 \
 	--train_size 6000000 \
@@ -30,7 +30,7 @@ nohup python ./t2t_bert/distributed_bin/tpu_train_eval_api.py \
 	--init_lr 5e-4 \
 	--do_train true \
 	--num_tpu_cores 8 \
-	--tpu_name "albert0" \
+	--tpu_name "albert1" \
 	--mode "pretrain" \
 	--seq_type "seq2seq" \
 	--mask_type "left2right" \
