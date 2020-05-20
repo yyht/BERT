14b4e43e6fdb03f0455ff36b4612fb15766693fe
天逸 <albert.xht@alibaba-inc.com>
2020-05-20 11:09:10 +0800

    add gatedcnn tpu pretrain

--- t2t_bert/pretrain_finetuning/export_bert_seq_lm.sh
+++ t2t_bert/pretrain_finetuning/export_bert_seq_lm.sh
@@ -1,11 +1,11 @@
 CUDA_VISIBLE_DEVICES="" python ./t2t_bert/pretrain_finetuning/export_api.py \
  --buckets "/data/xuht" \
- --config_file "/data/xuht/bert_as_gpt/bert_config_tiny.json" \
- --model_dir "bert_as_gpt/export" \
- --init_checkpoint "bert_as_gpt/model.ckpt-429685" \
- --model_output "bert_as_gpt/model.ckpt-429685" \
- --max_length 64 \
- --export_dir "bert_as_gpt/export/infer_inputs_64" \
+ --config_file "/data/xuht/chinese_gpt_tiny/bert_config_tiny.json" \
+ --model_dir "chinese_gpt_tiny/export" \
+ --init_checkpoint "chinese_gpt_tiny/model.ckpt-429680" \
+ --model_output "chinese_gpt_tiny/model.ckpt-429680" \
+ --max_length 128 \
+ --export_dir "chinese_gpt_tiny/export/infer_inputs_128" \
  --num_classes 2 \
  --input_target "" \
  --model_scope "bert" \
