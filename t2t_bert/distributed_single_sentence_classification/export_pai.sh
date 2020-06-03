f8903d24ccc9da320d03842b0aa1f0ac4c43de4e
天逸 <albert.xht@alibaba-inc.com>
2020-06-03 15:11:15 +0800

    fix

--- t2t_bert/distributed_single_sentence_classification/export_pai.sh
+++ t2t_bert/distributed_single_sentence_classification/export_pai.sh
@@ -10,11 +10,11 @@ fi
 zip -r ${model_zip} ${model_folder} -x "*.DS_Store,*.git*" 
 
 pai_command="
-pai -name tensorflow180 
+pai -name tensorflow180
 	-Dscript='file://${model_zip}'
 	-DentryFile='./BERT/t2t_bert/distributed_bin/export_api.py' 
-	-DgpuRequired=100
-	-DhyperParameters='file:///Users/xuhaotian/Desktop/my_work/BERT/t2t_bert/distributed_single_sentence_classification/bert_tiny_green_multiclass_export'
+	-DgpuRequired=0
+	-DhyperParameters='file:///Users/xuhaotian/Desktop/my_work/BERT/t2t_bert/distributed_single_sentence_classification/bert_base_green_multiclass_export'
 	-Dbuckets='oss://alg-misc/BERT/?role_arn=acs:ram::1265628042679515:role/yuefeng2&host=cn-hangzhou.oss-internal.aliyun-inc.com';
 "
 echo "${pai_command}"
--- t2t_bert/distributed_single_sentence_classification/pai_export.sh
+++ t2t_bert/distributed_single_sentence_classification/pai_export.sh
@@ -1,1 +1,1 @@
-sh ./BERT/t2t_bert/distributed_single_sentence_classification/export_pai.sh ~/Desktop/my_work/odps_clt_release_64_seccontent_official/bin/odpscmd ./BERT ~/Desktop/my_work/BERT.zip
+sh ./BERT/t2t_bert/distributed_single_sentence_classification/export_pai.sh ~/Desktop/my_work/odps_clt_release_64_seccontent/bin/odpscmd ./BERT ~/Desktop/my_work/BERT.zip
