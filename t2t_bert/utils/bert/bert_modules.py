108c5a23b1c17f0d2962ae87d1a254a0f504e959
天逸 <albert.xht@alibaba-inc.com>
2020-08-30 12:30:34 +0800

    add selection-attention utils

--- t2t_bert/utils/bert/bert_modules.py
+++ t2t_bert/utils/bert/bert_modules.py
@@ -1237,7 +1237,7 @@ def transformer_model(input_tensor,
 						attention_dropout_name = dropout_name + "/layer_%d/attention/self" % layer_idx
 					else:
 						attention_dropout_name = None
-					if layer_idx == 0:
+					if layer_idx in list(range(num_hidden_layers)):
 						structural_attentions_args = structural_attentions
 					else:
 						structural_attentions_args = "none"
